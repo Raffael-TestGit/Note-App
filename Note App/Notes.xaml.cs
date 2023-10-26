f05bb7834fdd27bd00b9cf32f5f6173a0af18a82
BayernNr1 <148634688+BayernNr1@users.noreply.github.com>
2023-10-26 20:12:17 +0200

    added search feature

--- Note App/Notes.xaml.cs
+++ Note App/Notes.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Note_App.Events;
+﻿using Note_App.Context;
+using Note_App.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static Azure.Core.HttpHeader;
 
 namespace Note_App
 {
@@ -21,6 +23,7 @@ namespace Note_App
     public partial class Notes : Window
     {
 
+        DataContext context = new();
         public Notes()
         {
             InitializeComponent();
@@ -29,6 +32,21 @@ namespace Note_App
         private void ok_Button_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void box_search_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var noteNames = context.Notes.Select(n => n.Name).ToList();
+
+            list_Box.Items.Clear();
+
+            foreach (var noteName in noteNames)
+            {
+                if (noteName.Contains(box_search.Text))
+                {
+                    list_Box.Items.Add(noteName);
+                }              
+            }
         }
     }
 }
